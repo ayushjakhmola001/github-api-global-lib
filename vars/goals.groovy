af2639ceca714cc91abb73553412abdc77c290b6
ayushjakhmola001 <97776930+ayushjakhmola001@users.noreply.github.com>
2022-01-20 19:35:03 +0530

    Update goals.groovy

--- vars/goals.groovy
+++ vars/goals.groovy
@@ -4,3 +4,15 @@ def clone(String branch_name,String Url) {
   git branch: 'ninja', credentialsId: 'c3a2c9c1-bce8-4fb5-9a0a-2830274c46e6', url: 'https://github.com/ayushjakhmola001/spring3hibernate.git'
   
 }
+def test (){
+sh 'mvn compile'
+sh 'mvn pmd:pmd'
+sh 'mvn checkstyle:checkstyle'
+}
+def report(){
+recordIssues(tools: [pmdParser()])
+recordIssues(tools: [checkStyle()])
+}
+def clean(){
+sh 'mvn clean'
+}
