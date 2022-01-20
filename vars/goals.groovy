c7769721c98517df969ae6d6361b01f3e9d7ee1b
ayushjakhmola001 <97776930+ayushjakhmola001@users.noreply.github.com>
2022-01-20 18:13:38 +0530

    Update goals.groovy

--- vars/goals.groovy
+++ vars/goals.groovy
@@ -1,6 +1,6 @@
 def clone(String branch_name,String Url) {
   echo "$branch_name"
   echo "$Url"
-  git branch: '${branch_name}', url: '${Url}'
+  git branch: 'ninja', credentialsId: 'c3a2c9c1-bce8-4fb5-9a0a-2830274c46e6', url: '${Url}
   
 }
