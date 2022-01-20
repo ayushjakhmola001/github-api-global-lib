cdf2ae701d83fe9514aafe773ccc5e2400973a3f
ayushjakhmola001 <97776930+ayushjakhmola001@users.noreply.github.com>
2022-01-20 18:01:12 +0530

    Update goals.groovy

--- vars/goals.groovy
+++ vars/goals.groovy
@@ -1,6 +1,6 @@
 def clone(String branch_name,String Url) {
   echo "$branch_name"
   echo "$Url"
-  git branch: '$branch_name', url: '$Url'
+  git branch: '${branch_name}', url: '${Url}'
+  
 }
-//git branch: 'ninja', url: 'https://github.com/ayushjakhmola001/spring3hibernate.git'
